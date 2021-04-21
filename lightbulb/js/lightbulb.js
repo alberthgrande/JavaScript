4b5caf27f43b257015854d42124a58a98b42c6a2
Alberth Ruado <31713127+alberthruado@users.noreply.github.com>
2021-04-21 16:24:54 -0700

    Add files via upload

---
+++ lightbulb/js/lightbulb.js
@@ -0,0 +1,15 @@
+
+        // sw stands for switch
+        function lightBulb(sw) {
+            var onOff;
+            if(sw == 0) {
+                onOff = "lightbulb/pic_bulboff.gif";
+            }
+            else {
+                onOff = "lightbulb/pic_bulbon.gif";
+            }
+            
+            // 'myImage' are came from the id of img element in html
+            document.getElementById('myImage').src = onOff;
+
+        }
