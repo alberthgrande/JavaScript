243c16d65026d514291596a57423bbdb72debdb7
alberthgrande <alberthgrande98@gmail.com>
2023-06-05 21:24:01 +0800

    Adde Defining a functions

--- Chapter 3 Functions/Defining_a_function.js
+++ Chapter 3 Functions/Defining_a_function.js
@@ -3,3 +3,18 @@ const square = function (x) {
 };
 
 console.log(square(12));
+
+const makeNoise = function () {
+  console.log("Pling!");
+};
+makeNoise();
+// → Pling!
+const power = function (base, exponent) {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+};
+console.log(power(2, 10));
+// → 1024
