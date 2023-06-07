d59d3dec7a378707ecd3fec756c5d5c9ee0e4dc5
alberthgrande <alberthgrande98@gmail.com>
2023-06-07 22:03:12 +0800

    Added new Lession to Chapter 3 Functions

---
+++ Chapter 3 Functions/Declaration_notation.js
@@ -0,0 +1,8 @@
+function square(x) {
+  return x * x;
+}
+
+function future() {
+  return "I'll have flying cars";
+}
+console.log("The future says:", future());
---
+++ Chapter 3 Functions/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+ingredient(1, "can", "chickpeas");
+  //   ingredient(0.25, "cup", "tahini");
+  //   ingredient(0.25, "cup", "lemon juice");
+  //   ingredient(1, "clove", "garlic");
+  //   ingredient(2, "tablespoon", "olive oil");
+  //   ingredient(0.5, "teaspoon", "cumin");
